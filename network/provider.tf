afa8d68b20849aa40228510e8fa127997a96737e
Yuki-TU <yuki@kakiuchiyukinoMacBook-Pro.local>
2025-03-15 19:25:30 +0900

    add local env

--- network/provider.tf
+++ network/provider.tf
@@ -1,17 +1,8 @@
 terraform {
   backend "s3" {
     region = "ap-northeast-1"
-    # ローカルの設定 start
-    access_key     = "minioadmin"
-    secret_key     = "minioadmin"
-    use_path_style = true
-    endpoints = {
-      s3  = "http://localhost:4566"
-      sts = "http://localhost:4566"
-    }
-    # ローカルの設定 end
   }
-  required_version = "1.8.3"
+  required_version = "1.11.2"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -29,17 +20,16 @@ provider "aws" {
     }
   }
   # ローカルの設定 start
-  access_key                  = "minioadmin"
-  secret_key                  = "minioadmin"
-  s3_use_path_style           = true
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-  endpoints {
-    # s3             = "http://s3.localhost.localstack.cloud:4566"
-    # s3             = "http://localhost:9000"
-    s3  = "http://localhost:4566"
-    ec2 = "http://localhost:4566"
+  s3_use_path_style           = var.env == "local" ? true : null
+  skip_credentials_validation = var.env == "local" ? true : null
+  skip_metadata_api_check     = var.env == "local" ? true : null
+  skip_requesting_account_id  = var.env == "local" ? true : null
+  dynamic "endpoints" {
+    for_each = var.env == "local" ? [1] : []
+    content {
+      s3  = "http://localhost:5001"
+      ec2 = "http://localhost:5001"
+    }
   }
   # ローカルの設定 end
 }
